c358133a2475f11e0ec33db0a6c898c865c8a6f5
Luka Ramishvili <luka.ramishvili@gmail.com>
2018-06-22 10:42:24 +0400

    config placeholders

--- config/services.php
+++ config/services.php
@@ -3,15 +3,15 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
-    |
+      |--------------------------------------------------------------------------
+      | Third Party Services
+      |--------------------------------------------------------------------------
+      |
+      | This file is for storing the credentials for third party services such
+      | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+      | default location for this type of information, allowing packages
+      | to have a conventional place to find your various credentials.
+      |
     */
 
     'mailgun' => [
@@ -34,5 +34,22 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+
+    'facebook' => [
+        'client_id' => env('FB_CLIENT_ID'),
+        'client_secret' => env('FB_CLIENT_SECRET'),
+        'redirect' => env('APP_URL', 'http://localhost').'/auth/oauth/facebook',
+    ],
+
+    'instagram' => [
+        'client_id' => env('INSTAGRAM_KEY'),
+        'client_secret' => env('INSTAGRAM_SECRET'),
+        'redirect' => env('APP_URL', 'http://localhost').'/auth/oauth/instagram',
+    ],
+
+    'ganalytics' => [
+        'account_id' => env('GA_ACCOUNT_ID'),
+    ],
+
 
 ];
